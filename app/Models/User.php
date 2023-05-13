76c1d07b5e55f8dc8c65da152c325da1803cef75
unknown <mutungaeric839@gmail.com>
2023-05-13 16:53:53 +0300

    modifing to save image for profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profileImage()
+{
+    return $this->profile_image ? asset('storage/' . $this->profile_image) : asset('images/default-avatar.png');
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Models\Matches;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MatchesController;
 
 /*
 |--------------------------------------------------------------------------
