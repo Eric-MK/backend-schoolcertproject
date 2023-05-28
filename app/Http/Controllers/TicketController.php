0ee6c5e7cbf70ff7a6da2e2232313bd9c118bcc1
unknown <mutungaeric839@gmail.com>
2023-05-28 12:17:46 +0300

    purchase working for ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,63 +11,41 @@ use Illuminate\Support\Facades\Validator;
 class TicketController extends Controller
 {
     public function purchase(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'quantity' => 'required|integer|min:1',
-        ]);
+{
+    $validator = Validator::make($request->all(), [
+        'quantity' => 'required|integer|min:1',
+    ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
-
-        $user = Auth::user();
-
-        $ticket = Ticket::where('user_id', $user->id)->first();
-
-        if ($ticket) {
-            return response()->json(['message' => 'Ticket already purchased'], 400);
-        }
-
-        $quantity = $request->input('quantity');
-        $totalPrice = $quantity * 10; // Assuming ticket price is $10
-
-        $ticket = new Ticket();
-        $ticket->user_id = $user->id;
-        $ticket->quantity = $quantity;
-        $ticket->total_price = $totalPrice;
-        $ticket->save();
-
-        return response()->json([
-            'message' => 'Ticket purchased successfully',
-            'ticket' => $ticket,
-        ], 201);
+    if ($validator->fails()) {
+        return response()->json($validator->errors(), 422);
     }
 
-    public function userTickets()
-    {
-        $user = Auth::user();
-        $ticket = Ticket::where('user_id', $user->id)->first();
+    $userId = $request->input('user_id'); // Assuming the user ID is passed in the request
+    $user = User::find($userId);
 
-        if (!$ticket) {
-            return response()->json(['message' => 'No ticket purchased'], 404);
-        }
-
-        return response()->json([
-            'ticket' => $ticket,
-        ], 200);
+    if (!$user) {
+        return response()->json(['message' => 'User not found'], 404);
     }
 
-    public function delete()
-    {
-        $user = Auth::user();
-        $ticket = Ticket::where('user_id', $user->id)->first();
+    $ticket = Ticket::where('user_id', $userId)->first();
 
-        if (!$ticket) {
-            return response()->json(['message' => 'No ticket purchased'], 404);
-        }
-
-        $ticket->delete();
-
-        return response()->json(['message' => 'Ticket deleted successfully']);
+    if ($ticket) {
+        return response()->json(['message' => 'Ticket already purchased'], 400);
     }
+
+    $quantity = $request->input('quantity');
+    $totalPrice = $quantity * 10; // Assuming ticket price is $10
+
+    $ticket = new Ticket();
+    $ticket->user_id = $userId;
+    $ticket->quantity = $quantity;
+    $ticket->total_price = $totalPrice;
+    $ticket->save();
+
+    return response()->json([
+        'message' => 'Ticket purchased successfully',
+        'ticket' => $ticket,
+    ], 201);
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Models\Matches;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TicketController;
 use App\Http\Controllers\MatchesController;
 use App\Http\Controllers\ProfileController;
 
