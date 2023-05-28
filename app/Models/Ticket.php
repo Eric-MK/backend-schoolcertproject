3f374efe4339a55b6de4be5336d1317d03121d68
unknown <mutungaeric839@gmail.com>
2023-05-28 10:07:07 +0300

    making a relationship with user table

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);//user() method uses the belongsTo() method to create a relationship between the Ticket and User models.
+    }
 }
+//a Ticket model can be associated with a User model by the user_id column on the User model.
