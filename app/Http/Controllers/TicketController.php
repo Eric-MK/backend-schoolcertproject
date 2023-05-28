ddee73775d339be57c9d9fac16c4fa70f3fc34ad
unknown <mutungaeric839@gmail.com>
2023-05-28 11:19:26 +0300

    ticket controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,47 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class TicketController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function purchase(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'quantity' => 'required|integer|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = Auth::user();
+
+        $ticket = Ticket::where('user_id', $user->id)->first();
+
+        if ($ticket) {
+            return response()->json(['message' => 'Ticket already purchased'], 400);
+        }
+
+        $quantity = $request->input('quantity');
+        $totalPrice = $quantity * 10; // Assuming ticket price is $10
+
+        $ticket = new Ticket();
+        $ticket->user_id = $user->id;
+        $ticket->quantity = $quantity;
+        $ticket->total_price = $totalPrice;
+        $ticket->save();
+
+        return response()->json([
+            'message' => 'Ticket purchased successfully',
+            'ticket' => $ticket,
+        ], 201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function userTickets()
     {
-        //
+        $user = Auth::user();
+        $ticket = Ticket::where('user_id', $user->id)->first();
+
+        if (!$ticket) {
+            return response()->json(['message' => 'No ticket purchased'], 404);
+        }
+
+        return response()->json([
+            'ticket' => $ticket,
+        ], 200);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function delete()
     {
-        //
-    }
+        $user = Auth::user();
+        $ticket = Ticket::where('user_id', $user->id)->first();
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+        if (!$ticket) {
+            return response()->json(['message' => 'No ticket purchased'], 404);
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $ticket->delete();
+
+        return response()->json(['message' => 'Ticket deleted successfully']);
     }
 }
